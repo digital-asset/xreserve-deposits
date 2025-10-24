edd7d29311f3700543b9751a9f6e622e33c2cbc8
Bradley Heather <148788681+bradley-da@users.noreply.github.com>
2025-10-24 18:37:29 -0400

    Add GitHub Pages deployment configuration

--- canton-deposit-ui/vite.config.ts
+++ canton-deposit-ui/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/xreserve-deposits/',
   plugins: [react()],
   build: {
     rollupOptions: {
